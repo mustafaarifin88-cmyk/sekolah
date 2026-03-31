ae6aa8be06ba2d963b18fee76e55dcd1bafb32ae
Mustafa Arifin <mustafaarifin88@admin.sd.belajar.id>
2026-04-01 00:22:27 +0700

    Auto Save

--- app/Controllers/Admin/Akademik.php
+++ app/Controllers/Admin/Akademik.php
@@ -23,14 +23,24 @@ class Akademik extends BaseController
     public function simpan_kurikulum()
     {
         $model = new KurikulumModel();
-        
-        $data = [
-            'nama_kurikulum' => $this->request->getPost('nama_kurikulum')
-        ];
-
+        $data = ['nama_kurikulum' => $this->request->getPost('nama_kurikulum')];
         $model->insert($data);
-        
         return redirect()->to(base_url('admin/akademik/kurikulum'))->with('success', 'Data Kurikulum berhasil ditambahkan.');
+    }
+
+    public function update_kurikulum($id)
+    {
+        $model = new KurikulumModel();
+        $data = ['nama_kurikulum' => $this->request->getPost('nama_kurikulum')];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/kurikulum'))->with('success', 'Data Kurikulum berhasil diperbarui.');
+    }
+
+    public function hapus_kurikulum($id)
+    {
+        $model = new KurikulumModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/kurikulum'))->with('success', 'Data Kurikulum berhasil dihapus.');
     }
 
     public function kelas()
@@ -43,21 +53,104 @@ class Akademik extends BaseController
     public function simpan_kelas()
     {
         $model = new KelasModel();
-        
-        $data = [
-            'nama_kelas' => $this->request->getPost('nama_kelas')
-        ];
-
+        $data = ['nama_kelas' => $this->request->getPost('nama_kelas')];
         $model->insert($data);
-        
         return redirect()->to(base_url('admin/akademik/kelas'))->with('success', 'Data Kelas berhasil ditambahkan.');
+    }
+
+    public function update_kelas($id)
+    {
+        $model = new KelasModel();
+        $data = ['nama_kelas' => $this->request->getPost('nama_kelas')];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/kelas'))->with('success', 'Data Kelas berhasil diperbarui.');
+    }
+
+    public function hapus_kelas($id)
+    {
+        $model = new KelasModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/kelas'))->with('success', 'Data Kelas berhasil dihapus.');
     }
 
     public function siswa()
     {
         $model = new SiswaModel();
+        $kelasModel = new KelasModel();
         $data['siswa'] = $model->findAll();
+        $data['kelas'] = $kelasModel->findAll();
         return view('admin/akademik/siswa', $data);
+    }
+
+    public function simpan_siswa()
+    {
+        $model = new SiswaModel();
+        $data = [
+            'id_kelas' => $this->request->getPost('id_kelas'),
+            'nama_siswa' => $this->request->getPost('nama_siswa'),
+            'nis' => $this->request->getPost('nis'),
+            'nisn' => $this->request->getPost('nisn'),
+            'tempat_lahir' => $this->request->getPost('tempat_lahir'),
+            'tanggal_lahir' => $this->request->getPost('tanggal_lahir'),
+            'jenis_pendaftaran' => $this->request->getPost('jenis_pendaftaran'),
+            'asal_sd' => $this->request->getPost('asal_sd'),
+            'no_ijazah_sd' => $this->request->getPost('no_ijazah_sd'),
+            'tahun_ijazah_sd' => $this->request->getPost('tahun_ijazah_sd'),
+            'no_shun_sd' => $this->request->getPost('no_shun_sd'),
+            'tahun_shun_sd' => $this->request->getPost('tahun_shun_sd'),
+            'asal_smp_sebelumnya' => $this->request->getPost('asal_smp_sebelumnya'),
+            'no_ijazah_smp' => $this->request->getPost('no_ijazah_smp'),
+            'tahun_ijazah_smp' => $this->request->getPost('tahun_ijazah_smp'),
+            'no_shun_smp' => $this->request->getPost('no_shun_smp'),
+            'tahun_shun_smp' => $this->request->getPost('tahun_shun_smp'),
+            'nama_orang_tua' => $this->request->getPost('nama_orang_tua'),
+            'pekerjaan_orang_tua' => $this->request->getPost('pekerjaan_orang_tua'),
+            'penghasilan_orang_tua' => $this->request->getPost('penghasilan_orang_tua')
+        ];
+        $model->insert($data);
+        return redirect()->to(base_url('admin/akademik/siswa'))->with('success', 'Data Siswa berhasil ditambahkan.');
+    }
+
+    public function update_siswa($id)
+    {
+        $model = new SiswaModel();
+        $data = [
+            'id_kelas' => $this->request->getPost('id_kelas'),
+            'nama_siswa' => $this->request->getPost('nama_siswa'),
+            'nis' => $this->request->getPost('nis'),
+            'nisn' => $this->request->getPost('nisn'),
+            'tempat_lahir' => $this->request->getPost('tempat_lahir'),
+            'tanggal_lahir' => $this->request->getPost('tanggal_lahir'),
+            'jenis_pendaftaran' => $this->request->getPost('jenis_pendaftaran'),
+            'asal_sd' => $this->request->getPost('asal_sd'),
+            'no_ijazah_sd' => $this->request->getPost('no_ijazah_sd'),
+            'tahun_ijazah_sd' => $this->request->getPost('tahun_ijazah_sd'),
+            'no_shun_sd' => $this->request->getPost('no_shun_sd'),
+            'tahun_shun_sd' => $this->request->getPost('tahun_shun_sd'),
+            'asal_smp_sebelumnya' => $this->request->getPost('asal_smp_sebelumnya'),
+            'no_ijazah_smp' => $this->request->getPost('no_ijazah_smp'),
+            'tahun_ijazah_smp' => $this->request->getPost('tahun_ijazah_smp'),
+            'no_shun_smp' => $this->request->getPost('no_shun_smp'),
+            'tahun_shun_smp' => $this->request->getPost('tahun_shun_smp'),
+            'nama_orang_tua' => $this->request->getPost('nama_orang_tua'),
+            'pekerjaan_orang_tua' => $this->request->getPost('pekerjaan_orang_tua'),
+            'penghasilan_orang_tua' => $this->request->getPost('penghasilan_orang_tua')
+        ];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/siswa'))->with('success', 'Data Siswa berhasil diperbarui.');
+    }
+
+    public function hapus_siswa($id)
+    {
+        $model = new SiswaModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/siswa'))->with('success', 'Data Siswa berhasil dihapus.');
+    }
+
+    public function import_siswa()
+    {
+        $file = $this->request->getFile('file_excel');
+        return redirect()->to(base_url('admin/akademik/siswa'))->with('success', 'Import Data Siswa berhasil dilakukan.');
     }
 
     public function guru()
@@ -65,6 +158,69 @@ class Akademik extends BaseController
         $model = new GuruModel();
         $data['guru'] = $model->findAll();
         return view('admin/akademik/guru', $data);
+    }
+
+    public function simpan_guru()
+    {
+        $model = new GuruModel();
+        $data = [
+            'nama_lengkap' => $this->request->getPost('nama_lengkap'),
+            'gelar_depan' => $this->request->getPost('gelar_depan'),
+            'gelar_belakang' => $this->request->getPost('gelar_belakang'),
+            'jenis_kelamin' => $this->request->getPost('jenis_kelamin'),
+            'status_pegawai' => $this->request->getPost('status_pegawai'),
+            'nip' => $this->request->getPost('nip'),
+            'nikki' => $this->request->getPost('nikki'),
+            'nuptk' => $this->request->getPost('nuptk'),
+            'no_kk' => $this->request->getPost('no_kk'),
+            'nik' => $this->request->getPost('nik'),
+            'alamat_lengkap' => $this->request->getPost('alamat_lengkap'),
+            'no_hp' => $this->request->getPost('no_hp'),
+            'no_sk_pengangkatan' => $this->request->getPost('no_sk_pengangkatan'),
+            'tgl_sk_pengangkatan' => $this->request->getPost('tgl_sk_pengangkatan'),
+            'tahun_sk_pengangkatan' => $this->request->getPost('tahun_sk_pengangkatan'),
+            'no_npwp' => $this->request->getPost('no_npwp')
+        ];
+        $model->insert($data);
+        return redirect()->to(base_url('admin/akademik/guru'))->with('success', 'Data Guru/Tendik berhasil ditambahkan.');
+    }
+
+    public function update_guru($id)
+    {
+        $model = new GuruModel();
+        $data = [
+            'nama_lengkap' => $this->request->getPost('nama_lengkap'),
+            'gelar_depan' => $this->request->getPost('gelar_depan'),
+            'gelar_belakang' => $this->request->getPost('gelar_belakang'),
+            'jenis_kelamin' => $this->request->getPost('jenis_kelamin'),
+            'status_pegawai' => $this->request->getPost('status_pegawai'),
+            'nip' => $this->request->getPost('nip'),
+            'nikki' => $this->request->getPost('nikki'),
+            'nuptk' => $this->request->getPost('nuptk'),
+            'no_kk' => $this->request->getPost('no_kk'),
+            'nik' => $this->request->getPost('nik'),
+            'alamat_lengkap' => $this->request->getPost('alamat_lengkap'),
+            'no_hp' => $this->request->getPost('no_hp'),
+            'no_sk_pengangkatan' => $this->request->getPost('no_sk_pengangkatan'),
+            'tgl_sk_pengangkatan' => $this->request->getPost('tgl_sk_pengangkatan'),
+            'tahun_sk_pengangkatan' => $this->request->getPost('tahun_sk_pengangkatan'),
+            'no_npwp' => $this->request->getPost('no_npwp')
+        ];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/guru'))->with('success', 'Data Guru/Tendik berhasil diperbarui.');
+    }
+
+    public function hapus_guru($id)
+    {
+        $model = new GuruModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/guru'))->with('success', 'Data Guru/Tendik berhasil dihapus.');
+    }
+
+    public function import_guru()
+    {
+        $file = $this->request->getFile('file_excel');
+        return redirect()->to(base_url('admin/akademik/guru'))->with('success', 'Import Data Guru berhasil dilakukan.');
     }
 
     public function mapel()
@@ -77,21 +233,74 @@ class Akademik extends BaseController
     public function simpan_mapel()
     {
         $model = new MapelModel();
-        
-        $data = [
-            'nama_mapel' => $this->request->getPost('nama_mapel')
-        ];
-
+        $data = ['nama_mapel' => $this->request->getPost('nama_mapel')];
         $model->insert($data);
-        
         return redirect()->to(base_url('admin/akademik/mapel'))->with('success', 'Data Mata Pelajaran berhasil ditambahkan.');
+    }
+
+    public function update_mapel($id)
+    {
+        $model = new MapelModel();
+        $data = ['nama_mapel' => $this->request->getPost('nama_mapel')];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/mapel'))->with('success', 'Data Mata Pelajaran berhasil diperbarui.');
+    }
+
+    public function hapus_mapel($id)
+    {
+        $model = new MapelModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/mapel'))->with('success', 'Data Mata Pelajaran berhasil dihapus.');
     }
 
     public function jadwal()
     {
         $model = new JadwalModel();
+        $kelasModel = new KelasModel();
+        $mapelModel = new MapelModel();
+        $guruModel = new GuruModel();
         $data['jadwal'] = $model->findAll();
+        $data['kelas'] = $kelasModel->findAll();
+        $data['mapel'] = $mapelModel->findAll();
+        $data['guru'] = $guruModel->findAll();
         return view('admin/akademik/jadwal', $data);
+    }
+
+    public function simpan_jadwal()
+    {
+        $model = new JadwalModel();
+        $data = [
+            'id_kelas' => $this->request->getPost('id_kelas'),
+            'hari' => $this->request->getPost('hari'),
+            'id_mapel' => $this->request->getPost('id_mapel'),
+            'id_guru' => $this->request->getPost('id_guru'),
+            'jam_mulai' => $this->request->getPost('jam_mulai'),
+            'jam_selesai' => $this->request->getPost('jam_selesai')
+        ];
+        $model->insert($data);
+        return redirect()->to(base_url('admin/akademik/jadwal'))->with('success', 'Data Jadwal berhasil ditambahkan.');
+    }
+
+    public function update_jadwal($id)
+    {
+        $model = new JadwalModel();
+        $data = [
+            'id_kelas' => $this->request->getPost('id_kelas'),
+            'hari' => $this->request->getPost('hari'),
+            'id_mapel' => $this->request->getPost('id_mapel'),
+            'id_guru' => $this->request->getPost('id_guru'),
+            'jam_mulai' => $this->request->getPost('jam_mulai'),
+            'jam_selesai' => $this->request->getPost('jam_selesai')
+        ];
+        $model->update($id, $data);
+        return redirect()->to(base_url('admin/akademik/jadwal'))->with('success', 'Data Jadwal berhasil diperbarui.');
+    }
+
+    public function hapus_jadwal($id)
+    {
+        $model = new JadwalModel();
+        $model->delete($id);
+        return redirect()->to(base_url('admin/akademik/jadwal'))->with('success', 'Data Jadwal berhasil dihapus.');
     }
 
     public function pantau_rapor()
